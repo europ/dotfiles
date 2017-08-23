782508a02d08022b7f0585fca66aae39a90e06a8
Adrian Toth <adricko28@gmail.com>
2017-08-23 13:33:40 +0200

    updater.sh

--- updater.sh
+++ updater.sh
@@ -8,17 +8,14 @@ DF="/home/adrian/dotfiles"
 MIQ="/home/adrian/ManageIQ/manageiq"
 MIQUC="/home/adrian/ManageIQ/manageiq-ui-classic"
 
-echo "${BOLD_YELLOW}Running \"git up\" in \"${DF}\"${RESET}"
-git -C "$DF" master && git -C "$DF" up
+echo "${BOLD_YELLOW}Running \"git f\" in \"${DF}\"${RESET}"
+git -C "$DF" master && git -C "$DF" f
 
-echo "${BOLD_YELLOW}Running \"git up\" in \"${MIQ}\"${RESET}"
-git -C "$MIQ" master && git -C "$MIQ" up
+echo "${BOLD_YELLOW}Running \"git f\" in \"${MIQ}\"${RESET}"
+git -C "$MIQ" master && git -C "$MIQ" f
 
-echo "${BOLD_YELLOW}Running \"git up\" in \"${MIQUC}\"${RESET}"
-git -C "$MIQUC" master && git -C "$MIQUC" up
-
-echo "${BOLD_YELLOW}Running \"bundler update\" in \"${MIQ}\"${RESET}"
-cd "$MIQ" && bundler install
+echo "${BOLD_YELLOW}Running \"git f\" in \"${MIQUC}\"${RESET}"
+git -C "$MIQUC" master && git -C "$MIQUC" f
 
 echo "${BOLD_YELLOW}Running \"bin/update\" in \"${MIQ}\"${RESET}"
 cd "$MIQ" && bin/update
