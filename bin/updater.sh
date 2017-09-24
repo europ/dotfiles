20f518d7f1763440846eb61812731abe9bcaf212
Adrian Toth <adricko28@gmail.com>
2017-09-24 20:21:37 +0200

    updater.sh

--- bin/updater.sh
+++ bin/updater.sh
@@ -7,15 +7,19 @@ REGULAR_YELLOW="\033[33m"
 DF="/home/adrian/dotfiles"
 MIQ="/home/adrian/ManageIQ/manageiq"
 MIQUC="/home/adrian/ManageIQ/manageiq-ui-classic"
+MIQBOT="/home/adrian/ManageIQ/miq_bot"
 
 echo "${BOLD_YELLOW}Running \"git pull\" in \"${DF}\"${RESET}"
 git -C "$DF" master && git -C "$DF" pull
 
 echo "${BOLD_YELLOW}Running \"git f\" in \"${MIQ}\"${RESET}"
-git -C "$MIQ" master && git -C "$MIQ" f
+git -C "$MIQ" f
 
 echo "${BOLD_YELLOW}Running \"git f\" in \"${MIQUC}\"${RESET}"
-git -C "$MIQUC" master && git -C "$MIQUC" f
+git -C "$MIQUC" f
+
+echo "${BOLD_YELLOW}Running \"git f\" in \"${MIQBOT}\"${RESET}"
+git -C "$MIQBOT" f
 
 echo "${BOLD_YELLOW}Running \"gem install bundler --pre\" in \"${MIQ}\"${RESET}"
 echo "${REGULAR_YELLOW}Current `cd "$MIQ" && bundler -v | awk '{print tolower($0)}'`${RESET}"
